07bcf63e585b9b0100a3bd278a845138f6bddf08
Svilen Stoicheff <svilen@yahoo.com>
2020-03-09 12:32:41 -0500

    Added standings table

--- src/App.js
+++ src/App.js
@@ -3,73 +3,105 @@ import logo from './logo.svg';
 import './App.css';
 
 class GetLeagues extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
       items: [],
-    }
+      teams: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     const options = {
       method: 'GET',
-      mode: 'no-cors'
+      headers: { "X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e" },
     };
-      const url = 'http://api.football-data.org/v2/competitions?plan=TIER_ONE'; 
-      fetch(url, { 
-        headers: {"X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e"}})
+    const url = 'http://api.football-data.org/v2/competitions?plan=TIER_ONE';
+    fetch(url, options)
       .then(response => response.json())
       .then((data) => {
-     this.setState({
-       isLoaded: true,
-       items: data.competitions
-     })
-  })
+        this.setState({
+          isLoaded: true,
+          items: data.competitions,
+          //teams: null,
+        })
+      })
+  }
+
+  handleClick(e) {
+    const competitionId = e.target.getAttribute('id');
+    console.log(e.target.getAttribute('id'));
+    console.log('this', this);
+    const options = {
+      method: 'GET',
+      headers: { "X-Auth-Token": "55e2b001494e4a19b5ea2aa10ada3c7e" },
+    };
+    const url = `http://api.football-data.org/v2/competitions/${competitionId}/standings`;
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ teams: data });
+      });
+  }
+
+  render() {
+    const items = this.state.items;
+    return (
+      <section className="standings-grid">
+        <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            <a href="#" id={item.id} onClick={this.handleClick}>
+              {item.area.name} - {item.name}
+            </a>
+          </li>
+        )
+        )}
+      </ul>
+        {console.log('before calling GetTeams', this.state.teams)}
+        <GetTeams teams={this.state.teams} />
+      </section>)
+  }
+
 }
 
-handleClick(e) {
-  console.log(e.target.getAttribute('id'));
-}
+class GetTeams extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-render () {
-  const items = this.state.items;
-    return (<ul>
-      {items.map(item => (
-        <li key={item.id}>
-          <a href="#"  id={item.id} onClick={this.handleClick}>
-          {item.area.name} - {item.name}
-          </a>
-        </li>
-      )
-    )}
-    </ul>)
-}  
-
+  render() {
+    console.log('render GetTeams');
+    if (this.props.teams) {
+      const teamStandings = this.props.teams.standings[0].table;
+      console.log('this.props.teams in GetTeams', this.props.teams);
+      return (<table className="standings">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>team</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teamStandings.map((team) => {
+            return (<tr><td>{team.position}</td><td>{team.team.name}</td></tr>);
+          })}
+        </tbody>
+      </table>);
+    }
+    console.log('this.props.teams in GetTeams', this.props.teams);
+    return (<div></div>);
+  }
 }
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <section>
+    <main>
         <GetLeagues />
-      </section>
-    </div>
+    </main>
   );
 }
 
